f549899d1239fce1b122d5c2046717ca63420c38
Paul Calnan <pcalnan71@gmail.com>
2023-01-08 08:09:22 +1100

    start restaurant cards list

---
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -0,0 +1,39 @@
+import React from "react";
+import styled from "styled-components/native";
+import { Text } from "react-native";
+import { Card } from "react-native-paper";
+
+const RestaurantCard = styled(Card)`
+  background-color: white;
+`;
+
+const RestaurantCardCover = styled(Card.Cover)`
+  padding: 20px;
+  background-color: white;
+`;
+
+const Title = styled(Text)`
+  padding: 16px;
+  color: red;
+`;
+
+export const RestaurantInfoCard = ({ restaurant = {} }) => {
+  const {
+    name = "Some Restaurant",
+    icon,
+    photos = [
+      "https://images1.androeed.ru/icon/2021/05/12/ico-kafe-panika-gastronomicheskii-restoran-1620861603.webp",
+    ],
+    address = "100 some random street",
+    isOpenNow = true,
+    rating = 4,
+    isClosedTemporarily,
+  } = restaurant;
+
+  return (
+    <RestaurantCard elevation={5}>
+      <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
+      <Title>{name}</Title>
+    </RestaurantCard>
+  );
+};
